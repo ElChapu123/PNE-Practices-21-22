e7941e18d11ecf9213f09f7855bd2ed7ce0c67de
ElChapu123 <a.antllaper@gmail.com>
2022-02-11 12:14:47 +0100

    q

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,8 +1,10 @@
 import seq0
 from pathlib import Path
+try:
+    file = input("Enter file name: ")
+    file = "./DNA_SEQ/" + file
 
-file = input("Enter file name: ")
-file = "./DNA_SEQ/" + file
-
-seq = Path(file).read_text()
-print("The first 20 bases are:\n" + seq0.read_seq_fasta(seq))
+    seq = Path(file).read_text()
+    print("The first 20 bases are:\n" + seq0.read_seq_fasta(seq))
+except FileNotFoundError:
+    print("File not found")
