b24a0d78cb6428fe1ffb0285cc298569b91820cf
ElChapu123 <a.antllaper@gmail.com>
2022-02-23 18:33:38 +0100

    º

--- Theory/Servers.py
+++ Theory/Servers.py
@@ -1,8 +1,8 @@
 import socket
 
 # Configure the Server's IP and PORT
-PORT = 8081
-IP = "192.168.1.36"
+PORT = 21000
+IP = "0.0.0.0"
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections
@@ -32,7 +32,7 @@ try:
         print("Message from client: {}".format(msg))
 
         # Send the messag
-        message = "Hello from the teacher's server"
+        message = "Hello from the teacher's server "
         send_bytes = str.encode(message)
         # We must write bytes, not a string
         clientsocket.send(send_bytes)
