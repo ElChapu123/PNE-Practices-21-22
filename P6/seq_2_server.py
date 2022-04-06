8f88a3763bbb6dade1bd538b8990407ec14462c5
ElChapu123 <a.antllaper@gmail.com>
2022-04-06 18:59:55 +0200

    º

--- P6/seq_2_server.py
+++ P6/seq_2_server.py
@@ -2,8 +2,11 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
+from seq1 import Seq
+
 # Define the Server's port
 PORT = 8080
+
 
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
@@ -17,41 +20,67 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         """This method is called whenever the client invokes the GET method
         in the HTTP protocol request"""
 
+        seq_list = ["ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA","AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA","CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT","CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA","AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT"]
+
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
 
-        # IN this simple server version:
-        # We are NOT processing the client's request
-
-        # Message to send back to the client
         try:
             route = self.requestline.split(" ")[1]
             filename = route[1:]
+            print("FILENAME", filename)
         except IndexError:
             route = "/"
+
+        try:
+            filename = filename.split("?")[1]
+            filename = filename.split("&")
+
+            cmd_dict = {}
+
+            for e in filename:
+                e = e.split("=")
+                cmd_dict[e[0]] = e[1]
+            print(cmd_dict)
+
+        except IndexError:
+            pass
+
+
+
+        termcolor.cprint(filename, "blue")
+
+
 
         if route == "/":
             contents = Path("./html/index.html").read_text()
         elif route == "/favicon.ico":
             contents = Path("./html/index.html").read_text()
+
+        elif route.startswith("/ping"):
+            contents = Path("./html/PING.html").read_text()
+
+        elif route.startswith("/get?"):
+            n = int(cmd_dict["number"])
+            seq = seq_list[int(cmd_dict["number"])]
+            contents = Path("./html/GET.html").read_text().format(n=n, seq=seq)
+
         else:
-            try:
-                contents = Path("./html/" + filename + ".html").read_text()
-            except FileNotFoundError:
-                contents = Path("html/error.html").read_text()
+            contents = Path("html/error.html").read_text()
 
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
 
         # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(contents.encode()))
+        self.send_header('Content-Length', len(str.encode(contents)))
 
         # The header is finished
         self.end_headers()
 
         # Send the response message
-        self.wfile.write(contents.encode())
+        self.wfile.write(str.encode(contents))
+
         return
 
 
